b74cfda8afe46eb98366ca113534ddd437e9c179
joao <joaopedroazevedo1980@gmail.com>
2026-05-08 20:35:05 -0300

    Sinal

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int zero;
+Console.WriteLine("Digite um número: ");
+int numeropositivo = Convert.ToInt32(Console.ReadLine());
+
+if (numeropositivo >0)
+{
+    Console.WriteLine("Positivo");
+}
+else if (numeropositivo <0)
+{
+    Console.WriteLine("Negativo");
+}
+else
+{
+    Console.WriteLine("zero");
+}
